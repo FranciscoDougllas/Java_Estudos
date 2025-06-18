e2ba32b607ea78cd79ef013c1f91d171dc3cbea2
FranciscoDougllas <franciscodouglas77@outlook.com>
2025-06-18 17:54:29 -0300

    -------

--- 05_OrientacaoObjetos/src/pagamentos/folhas.java
+++ 05_OrientacaoObjetos/src/pagamentos/folhas.java
@@ -3,7 +3,7 @@ import empresa.Funcionario;
 
 public class folhas {
     public static void main(String[] args) {
-        Funcionario funcionario = new Funcionario("João", 1000,
+        Funcionario funcionario = new Funcionario("João", 1200,
                 25, "Programador");
     }
 }
